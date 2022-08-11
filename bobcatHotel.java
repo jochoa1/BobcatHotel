5a308d2cb206279b1d7406ed61f5ef1664b10466
jochoa1 <ochoa8587@gmail.com>
2022-08-11 16:10:15 -0700

    try again

--- bobcatHotel.java
+++ bobcatHotel.java
@@ -6,5 +6,23 @@ public class bobcatHotel{
     public static void main(String[] args){
 
         Scanner input = new Scanner(System.in);
+        
+
+        int yes = 1 , no = 1; 
+        double roomCost = 0;
+        double mealCost = 0;
+        double memberDiscount = 0;
+        double aaaDiscount = 0;
+
+        System.out.println("ROOM OPTIONS");
+        System.out.println("1. Single @ $50.50 per night");
+		System.out.println("2. Double @ $75.00 per night");
+		System.out.println("3. Queen @ 100.75 per night");
+		System.out.println("4. King @ $150.25 per night");
+		System.out.println("5. Master Suite @ $225.50 per night");
+
+        System.out.println();
+
+
     }
 }
